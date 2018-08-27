8617f8b005a9e75529444c5b7b9063741d3522c5
Hay Kranen <huskyr@gmail.com>
2018-08-27 23:18:01 +0200

    Adding a template

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const fs = require('fs');
 const pathlib = require('path');
 const globby = require('globby');
 const marked = require('marked');
+const handlebars = require('handlebars');
 
 const pkg = fs.readFileSync('./package.json', 'utf-8');
 const pkgData = JSON.parse(pkg);
+
+const templateFile = fs.readFileSync('./template/template.html', 'utf-8');
+const template = handlebars.compile(templateFile);
 
 const Parser = require('argparse').ArgumentParser;
 
@@ -19,19 +23,24 @@ async function main(args) {
     const paths = await globby(args.path);
 
     paths.forEach((path) => {
-        let html;
+        let htmlContent;
         const parsedPath = pathlib.parse(path);
         const ext = parsedPath.ext.slice(1);
 
         if (ext === 'md') {
             const text = fs.readFileSync(path, 'utf-8');
-            html = marked(text);
+            htmlContent= marked(text);
         }
 
-        if (html) {
+        if (htmlContent) {
             delete parsedPath.base;
             parsedPath.ext = '.html';
             const htmlPath = pathlib.format(parsedPath);
+
+            const html = template({
+                content : htmlContent
+            });
+
             fs.writeFileSync(htmlPath, html);
         }
     });
