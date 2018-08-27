768000ffbe56dc44bbf4d4e49a422c713771b818
Hay Kranen <huskyr@gmail.com>
2018-08-27 23:34:51 +0200

    Refactoring stuff into /lib

--- index.js
+++ index.js
@@ -1,50 +1,21 @@
 #!/usr/bin/env node
-const fs = require('fs');
-const pathlib = require('path');
-const globby = require('globby');
-const marked = require('marked');
-const handlebars = require('handlebars');
+const { readJsonFile } = require('./lib/util.js');
+const Dexify = require('./lib/dexify.js');
 
-const pkg = fs.readFileSync('./package.json', 'utf-8');
-const pkgData = JSON.parse(pkg);
-
-const templateFile = fs.readFileSync('./template/template.html', 'utf-8');
-const template = handlebars.compile(templateFile);
+const pkg = readJsonFile('./package.json');
 
 const Parser = require('argparse').ArgumentParser;
 
 const parser = new Parser({
-    description : pkgData.description
+    description : pkg.description
 });
 
 parser.addArgument('path', { help : 'Path to file(s)'});
-
-async function main(args) {
-    const paths = await globby(args.path);
-
-    paths.forEach((path) => {
-        let htmlContent;
-        const parsedPath = pathlib.parse(path);
-        const ext = parsedPath.ext.slice(1);
-
-        if (ext === 'md') {
-            const text = fs.readFileSync(path, 'utf-8');
-            htmlContent= marked(text);
-        }
-
-        if (htmlContent) {
-            delete parsedPath.base;
-            parsedPath.ext = '.html';
-            const htmlPath = pathlib.format(parsedPath);
-
-            const html = template({
-                content : htmlContent
-            });
-
-            fs.writeFileSync(htmlPath, html);
-        }
-    });
-}
+parser.addArgument(['-t', '--template'], {
+    defaultValue : './template/template.html',
+    help : 'Path to the Handlebars template file'
+})
 
 const args = parser.parseArgs();
-main(args);
+const dexify = new Dexify(args);
+dexify.run();
