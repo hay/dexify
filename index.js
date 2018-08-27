6e8e0cfc325a23c5fda55a68f129258a6f3c1434
Hay Kranen <huskyr@gmail.com>
2018-08-27 23:53:12 +0200

    Added some logging

--- index.js
+++ index.js
@@ -7,15 +7,26 @@ const pkg = readJsonFile('./package.json');
 const Parser = require('argparse').ArgumentParser;
 
 const parser = new Parser({
-    description : pkg.description
+    description : pkg.description,
+    prog : pkg.name
 });
 
 parser.addArgument('path', { help : 'Path to file(s)'});
 parser.addArgument(['-t', '--template'], {
     defaultValue : './template/template.html',
-    help : 'Path to the Handlebars template file'
-})
+    help : 'Path to the Handlebars template file (defaults to built-in template)'
+});
+parser.addArgument(['-v', '--verbose'], {
+    action : 'storeTrue',
+    dest : 'debug',
+    help : 'Print debug information'
+});
 
-const args = parser.parseArgs();
-const dexify = new Dexify(args);
-dexify.run();
+// Check if we have got any arguments, otherwise print help
+if (process.argv.length == 2) {
+    parser.printHelp();
+} else {
+    const args = parser.parseArgs();
+    const dexify = new Dexify(args);
+    dexify.run();
+}
